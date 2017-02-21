cf96ac3aefcdb4cb7ad2b31637cf46c1fec586c6
JOSHNEWLAPTOP\Josh <jemcpherson@mix.wvu.edu>
2017-02-21 00:33:45 -0500

    Set appropriate background color

--- src/game.py
+++ src/game.py
@@ -10,6 +10,6 @@ class Game(object):
 		pygame.font.init()
 		self.myfont = pygame.font.SysFont('Arial',30)
 		self.scoreSurface = self.myfont.render("Score: " + str(self.score),True,(255,255,255))
-		self.background_color = (0,0,0)
+		self.background_color = (112,132,55)
 		self.car = Car()
 		self.road = Road(height)
