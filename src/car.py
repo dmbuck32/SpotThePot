2a29e6a3d0508710dbba0772b5f2d8a084db377f
JOSHNEWLAPTOP\Josh <jemcpherson@mix.wvu.edu>
2017-02-20 21:57:08 -0500

    Worked a very little bit on getting stuff started. If we keep track of the instances of all our objects in the game object (game.py) we can access everything anywhere we need by just traversing game's variables (this is why I have game passing in to draw, update, and handle_input). We don't have to do it this way if someone has a better way of doing it, this is just what I figured would be straightforward.

--- src/car.py
+++ src/car.py
@@ -5,10 +5,10 @@ class Car(object):
 	def __init__(self):
 		self.x = 280
 		self.y = 280
-		self.width = 30
-		self.height = 30
+		self.width = 100
+		self.height = 100
 		self.rect = pygame.Rect(self.x,self.y,self.width,self.height)
-		self.image = pygame.image.load('car.png').convert_alpha()
+		self.image = pygame.transform.scale(pygame.image.load('car.png').convert_alpha(),(self.width,self.height))
 	
 	def moveLeft(self):
 		if(self.x > 0): 
