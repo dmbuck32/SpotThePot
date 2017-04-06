95ed733391468dcb69efd33a8ee195c9c23df5f1
natrefz <natrefz@mix.wvu.edu>
2017-04-06 14:46:13 -0400

    Added the lives png files to the images folder and implemented them in the top left corner in the game.

--- src/game.py
+++ src/game.py
@@ -11,13 +11,14 @@ class Game(object):
 		self.lives = 4
 		self.level = 1
 		self.minTime = 200
-		self.maxTime = 280
+		self.maxTime = 300
 		pygame.font.init()
 		self.myfont = pygame.font.SysFont('Arial',30)
 		self.scoreSurface = self.myfont.render("Score: " + str(self.score),True,(255,255,255))
 		self.livesSurface = self.myfont.render("Lives: " + str(self.lives),True,(255,255,255))
+		self.livesImage = pygame.transform.scale(pygame.image.load('Images/4lives.png').convert_alpha(),(65,120))
 		self.gameOverSurface = self.myfont.render("Game Over",True,(255,255,255))
-		self.levelSurface = self.myfont.render("Level "+ str(self.level), True, (255,255,255))
+		self.levelSurface = self.myfont.render("Level: "+ str(self.level), True, (255,255,255))
 		self.levelUpMessageSurface = self.myfont.render("", True, (255,215, 0))
 		self.bonusLifeSurface = self.myfont.render("", True, (255, 215, 0))
 		self.background_color = (112,132,55)
@@ -43,22 +44,41 @@ class Game(object):
 		
 	def updateLives(self):
 		self.lives -= 1
+		livesImageWidth = 65
+		livesImageHeight = 120
+		if self.lives >= 4:
+			self.livesImage = pygame.transform.scale(pygame.image.load('Images/4lives.png').convert_alpha(),(livesImageWidth,livesImageHeight))
+		elif self.lives == 3:
+			self.livesImage = pygame.transform.scale(pygame.image.load('Images/3lives.png').convert_alpha(),(livesImageWidth,livesImageHeight))
+		elif self.lives == 2:
+			self.livesImage = pygame.transform.scale(pygame.image.load('Images/2lives.png').convert_alpha(),(livesImageWidth,livesImageHeight))
+		elif self.lives == 1:
+			self.livesImage = pygame.transform.scale(pygame.image.load('Images/1life.png').convert_alpha(),(livesImageWidth,livesImageHeight))
+		elif self.lives ==0:
+			self.livesImage = pygame.transform.scale(pygame.image.load('Images/0lives.png').convert_alpha(),(livesImageWidth,livesImageHeight))
+			
 		self.livesSurface = self.myfont.render("Lives: " + str(self.lives),True,(255,255,255))
 		
 	def updateLevel(self):
-		if ((self.score > 0) and (self.score % 10 == 0)):
+		if ((self.score > 0) and (self.score % 15 == 0)):
 			self.level += 1
 			self.levelUpMessageSurface = self.myfont.render("NEXT LEVEL!", True, (255,215, 0))
-			if self.level > 1 and self.level % 5 == 0:
-				self.lives += 1 # Bonus life every 5 levels
+			if self.level > 1 and self.level % 5 == 0 and self.lives < 4:
+				self.lives += 1 # Bonus life every 5 levels, if they can take it
 				self.bonusLifeSurface = pygame.font.SysFont('Aria', 25).render("Bonus Life!", True, (255,215,0))
 			else: 
 				self.bonusLifeSurface = self.myfont.render("", True, (255,215,0))
-			if self.minTime >50:
-				self.minTime -= 25
-				self.maxTime -= 30
+			if self.minTime == 200:
+				self.minTime -= 100
+				self.maxTime -= 100
+			elif self.minTime == 100:
+				self.minTime -= 50
+				self.maxTime -= 50
+			elif self.minTime <= 50:
+				self.minTime -= 10
+				self.maxTime -= 15
 			self.levelSurface = self.myfont.render("Level: "+ str(self.level), False, (255,255,255))
-			print "minTime: %d\nmaxTime: %d" %(self.minTime, self.maxTime)
+			# print "minTime: %d\nmaxTime: %d" %(self.minTime, self.maxTime)
 		else:
 			self.levelUpMessageSurface = self.myfont.render("", True, (255,215, 0))
 			self.bonusLifeSurface = self.myfont.render("", True, (255,215,0))
